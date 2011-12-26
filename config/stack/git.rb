706586ebe3b674c999b46c51e6fe727340761680
Leon Miller-Out <leon@singlebrook.com>
2011-12-25 22:20:24 -0500

    Add github ssh keys for all users on system
    Conflicts:
    
    	config/stack/git.rb

--- config/stack/git.rb
+++ config/stack/git.rb
@@ -2,8 +2,8 @@ package :git, :provides => :scm do
   description 'Git Distributed Version Control'
   version '1.7.7.5'
   source "http://git-core.googlecode.com/files/git-#{version}.tar.gz"
-  requires :git_dependencies
-  
+  requires :git_dependencies, :github_keys
+
   verify do
     has_file '/usr/local/bin/git'
   end
@@ -12,4 +12,10 @@ end
 package :git_dependencies do
   description 'Git Build Dependencies'
   apt 'git-core', :dependencies_only => true
+end
+
+package :github_keys do
+  github_key = File.read(File.join(File.dirname(__FILE__), 'git', 'github_key.txt'))
+  # Don't require verification of github's ssh fingerprint for anyone on the system
+  push_text github_key, '/etc/ssh/ssh_known_hosts', :sudo => true
 end
