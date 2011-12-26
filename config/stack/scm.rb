3c43db25e8673ee5175274a40647feaded72d1f4
Leon Miller-Out <leon@singlebrook.com>
2011-12-25 21:55:14 -0500

    Install git from apt instead of source to save time on Oneiric

--- config/stack/scm.rb
+++ config/stack/scm.rb
@@ -1,15 +1,9 @@
 package :git, :provides => :scm do
   description 'Git Distributed Version Control'
-  version '1.7.7.5'
-  source "http://git-core.googlecode.com/files/git-#{version}.tar.gz"
-  requires :git_dependencies
+
+  apt 'git'
   
   verify do
-    has_file '/usr/local/bin/git'
+    has_file '/usr/bin/git'
   end
-end
-
-package :git_dependencies do
-  description 'Git Build Dependencies'
-  apt 'git-core', :dependencies_only => true
 end
